5eee46f9e1f6bea2cab97e1c12fe0030505b9ef8
ZWally <zach.wallenburg@icloud.com>
2024-01-12 14:57:13 -0600

    socketio connection

--- back-end/index.ts
+++ back-end/index.ts
@@ -1,10 +1,30 @@
 import express from "express";
 import { createServer } from "http";
-
+import { Server } from "socket.io";
 
 const app = express();
 const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:5173"
+    }
+});
+
+
+
+io.on("connection", (socket) => {
+  console.log("Client connected:", socket.id);
+
+  socket.on("message", (msg) => {
+    console.log("Received message from client:", msg);
+    socket.emit("message", "Hello, client!");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
+});
 
 httpServer.listen(3001, () => {
-    console.log("Server listening on port 3001");
-  });
+  console.log("Server listening on port 3001");
+});
--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,8 +1,19 @@
 
+import { io } from 'socket.io-client';
 import './App.css'
+import { SERVER_PATH } from "../../shared/environment"
 import GuessWhoCharacter from './GuessWhoCharacter'
+import { useEffect } from 'react';
+
+const socket = io(SERVER_PATH);
 
 function App() {
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log("connected successfully")
+    });
+  }, []);
 
   return (
     <>
