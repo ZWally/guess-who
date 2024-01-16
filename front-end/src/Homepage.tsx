2c6ea3e6dd3ef4c9e9ce69a9ed811aeb5da020a6
ZWally <zach.wallenburg@icloud.com>
2024-01-15 19:28:45 -0600

    create homepage component

---
+++ front-end/src/Homepage.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+
+
+const Homepage: React.FC = () => {
+    const [name, setName]: [String, Function] = useState("")
+    const onCreateRoom = (name: String)=>{
+        //todo
+    }
+
+
+    return(
+        <div></div>
+    )
+} 
+export default Homepage;
